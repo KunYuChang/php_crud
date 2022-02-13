944bd74089cf9610b0efaebc371b28ff6c9df44e
KunYuChang <mischiefsub@gmail.com>
2022-02-14 04:47:01 +0800

    + read.php

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
+# 簡單的 PHP CRUD 實作
 
-khanam
+## 筆記
+
+1.  mysqli_fetch_assoc 一次只會輸出一行，因此需要使用 while 儲存陣列，而不能直接使用 foreach
---
+++ read.php
@@ -0,0 +1,57 @@
+<?php
+include_once 'connect.php';
+
+// select query
+$sql = "SELECT * FROM user";
+$result = mysqli_query($con, $sql);
+while ($row = mysqli_fetch_assoc($result)) {
+    $rows[] = $row;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Display data</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet">
+
+</head>
+
+<body>
+    <div class="container my-5">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Serial Number</th>
+                    <th scope="col">First Name</th>
+                    <th scope="col">Last Name</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Mobile</th>
+                    <th scope="col">Options</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($rows as $row) : ?>
+                    <tr>
+                        <td><?php echo $row['id']; ?></td>
+                        <td><?php echo $row['fname']; ?></td>
+                        <td><?php echo $row['lname']; ?></td>
+                        <td><?php echo $row['email']; ?></td>
+                        <td><?php echo $row['mobile']; ?></td>
+                        <td>
+                            <a href="#" class="btn btn-dark">Update</a>
+                            <a href="#" class="btn btn-danger">Delete</a>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+</body>
+
+</html>
